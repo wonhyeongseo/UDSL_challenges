7799bb0d99ddd31af786c8fab0482408d1d05e54
wonhyeongseo <pesfy101@gmail.com>
2018-05-31 08:05:41 +0900

    Update sort_by_xor.py

--- sort_by_xor.py
+++ sort_by_xor.py
@@ -1,6 +1,6 @@
 # v0.0.1
 
-def sort_by_word_length_xor(*t):
+def sort_by_xor(*t):
     """
     Get an iterable full of strings.
     Return a sorted list by:
@@ -12,15 +12,15 @@ def sort_by_word_length_xor(*t):
     """
     from collections import defaultdict
     dd = defaultdict(list)
-    for word in t:
-        dd[len(word)].append(word)
+    for string in t:
+        dd[len(string)].append(string)
     result = []
     for key in sorted(dd, reverse=True):
         result.extend(sorted(dd[key]))
     return result
 
 # testing sort_by_word_length_xor
-if sort_by_word_length_xor('gray', 'green', 'brown', 'red', 'blue') == ['brown', 'green', 'blue', 'gray', 'red']:
+if sort_by_xor('gray', 'green', 'brown', 'red', 'blue') == ['brown', 'green', 'blue', 'gray', 'red']:
     print("sorting has succeeded for request!")
 else:
     print("please check code")
